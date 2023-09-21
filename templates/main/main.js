0f7686e07a6d7a92357aefdff1969da6129e7de9
Juan Ceballo <edu17290@gmail.com>
2023-09-21 10:10:42 -0300

    ft: funcionalidad para traer los servidores del usuario que inicia sesión en main.js

--- templates/main/main.js
+++ templates/main/main.js
@@ -6,6 +6,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   getProfile();
 });
 
+// Funcion para hacer el GET de los datos del usuario que inica sesión
 function getProfile() {
   const URL = "http://127.0.0.1:5000/auth/profile";
   return fetch(URL, {
@@ -26,11 +27,33 @@ function getProfile() {
     });
 }
 
-// la función devuelve una promesa, por lo que manejamos la función como tal para usar el id y hacer las otras peticiones luego
+// LLamamos la función, como devuelve una promesa con el id del usuario entonces aqui usamos las otras peticiones
 getProfile()
   .then((user_id) => {
     console.log(user_id);
+    getServidores(user_id)
   })
   .catch((error) => {
     console.log(error);
   });
+
+// Obtenemos los servidores del usuario que inica sesión
+function getServidores(user_id) {
+  const URL = `http://127.0.0.1:5000/api/user_server/${user_id}`;
+  fetch(URL, {
+    method: "GET",
+    credentials: "include",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error en la solicitud");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
