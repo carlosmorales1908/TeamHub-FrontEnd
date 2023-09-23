cee847696bdbaf8a62cc9dc398e3fc7c805e3531
Juan Ceballo <edu17290@gmail.com>
2023-09-23 19:12:17 -0300

    ft: se empezó con la lógica del user profile

--- templates/user_profile/user_profile.js
+++ templates/user_profile/user_profile.js
@@ -1,13 +1,65 @@
-const d = document;
-const $changePassword = d.querySelector(".change-password");
-const $cerrarModal = d.querySelector(".close-modal");
-const $modal = d.querySelector(".nuevo-modal");
+const changePassword = document.querySelector(".change-password");
+const cerrarModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".nuevo-modal");
+const form = document.querySelector(".user-form")
 
-$changePassword.addEventListener("click",e=>{
+const inputUserName = document.querySelector(".inp-user-name")
+const inputName = document.querySelector(".inp-name")
+const inputLName = document.querySelector(".inp-lname")
+const inputEmail = document.querySelector(".inp-email")
+const inputCurrentPass = document.querySelector(".inp-current-pass")
+
+document.addEventListener("DOMContentLoaded",e=>{
+  const URL = `http://127.0.0.1:5000/auth/profile`;
+  fetch(URL, {
+    method: "GET",
+    credentials: "include",
+  })
+  .then(res=>res.ok?res.json():Promise.reject(res))
+    .then((data) => {
+      inputUserName.value = data.user_name
+      inputUserName.disabled = true
+      inputName.value = data.first_name
+      inputName.disabled = true
+      inputLName.value = data.last_name
+      inputLName.disabled = true
+      inputEmail.value = data.email
+      inputEmail.disabled = true
+      inputCurrentPass.value = data.password
+    })
+})
+
+//        TODO
+/*
+form.addEventListener('submit', e=>{
   e.preventDefault()
-  $modal.showModal()
+  console.log("quisiste guardar los cambios")
+  const formData = new FormData(form),
+    name = formData.get("name"),
+    lName = formData.get("lname"),
+    email = formData.get("email"),
+    userName = formData.get("user-name")
+
+    console.log(name)
+    console.log(lName)
+    console.log(email)
+    console.log(userName)
+  })
+*/
+
+document.addEventListener("click",e=>{
+  e.preventDefault()
+  if (e.target.matches("button")){
+    e.target.previousElementSibling.disabled = false
+  }
 })
 
-$cerrarModal.addEventListener("click",e=>{
-  $modal.close()
+
+changePassword.addEventListener("click",e=>{
+  e.preventDefault()
+  modal.showModal()
 })
+
+// cerrarModal.addEventListener("click",e=>{
+//   modal.close()
+// })
