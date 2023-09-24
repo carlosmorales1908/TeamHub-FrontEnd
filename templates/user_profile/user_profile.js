6036806e52f872a5894f09c21f7799e247762704
Juan Ceballo <edu17290@gmail.com>
2023-09-24 11:53:33 -0300

    ft: se termino de añadir la funcinalidad al modificar un usuario en las vistas VerPerfil y CambiarContraseña

--- templates/user_profile/user_profile.js
+++ templates/user_profile/user_profile.js
@@ -1,84 +1,125 @@
-const changePassword = document.querySelector(".change-password");
-const cerrarModal = document.querySelector(".close-modal");
+// const changePassword = document.querySelector(".change-password");
+// const cerrarModal = document.querySelector(".close-modal");
+// const form = document.querySelector(".user-form");
 const modal = document.querySelector(".nuevo-modal");
-const form = document.querySelector(".user-form")
+const errorMessage = document.querySelector(".error-message");
 
-const inputUserName = document.querySelector(".inp-user-name")
-const inputName = document.querySelector(".inp-name")
-const inputLName = document.querySelector(".inp-lname")
-const inputEmail = document.querySelector(".inp-email")
-const inputCurrentPass = document.querySelector(".inp-current-pass")
-const inputNewPass = document.querySelector(".new-pass")
+const inputUserName = document.querySelector(".inp-user-name");
+const inputName = document.querySelector(".inp-name");
+const inputLName = document.querySelector(".inp-lname");
+const inputEmail = document.querySelector(".inp-email");
+const inputCurrentPass = document.querySelector(".inp-current-pass");
+const inputNewPass = document.querySelector(".new-pass");
+const inputConfirmPass = document.querySelector(".confirm-new-pass");
 
-document.addEventListener("DOMContentLoaded",e=>{
+function getUserData() {
   const URL = `http://127.0.0.1:5000/auth/profile`;
-  fetch(URL, {
+  return fetch(URL, {
     method: "GET",
     credentials: "include",
   })
-  .then(res=>res.ok?res.json():Promise.reject(res))
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
     .then((data) => {
-      inputUserName.value = data.user_name
-      inputUserName.disabled = true
-      inputName.value = data.first_name
-      inputName.disabled = true
-      inputLName.value = data.last_name
-      inputLName.disabled = true
-      inputEmail.value = data.email
-      inputEmail.disabled = true
-      inputCurrentPass.value = data.password
+      return data;
     })
-})
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-//        TODO
+function updateUser(user_id) {
+  const url = `http://127.0.0.1:5000/api/users/${user_id}`;
+  console.log(url);
 
-document.addEventListener("click",e=>{
-  e.preventDefault()
-  if (e.target.matches(".btn-editar")){
-    e.target.previousElementSibling.disabled = false
+  const dataToSend = {
+    first_name: inputName.value,
+    last_name: inputLName.value,
+    email: inputEmail.value,
+    user_name: inputUserName.value,
+  };
+
+  if (inputNewPass.value === "" && inputConfirmPass.value === "") {
+    dataToSend.password = inputCurrentPass.value;
+  } else if (inputNewPass.value === inputConfirmPass.value) {
+    dataToSend.password = inputNewPass.value;
   }
 
-  //  FALTA CAPTURAR EL ID
-  if (e.target.matches(".get-data")){
-    const url = 'http://127.0.0.1:5000/api/users/id';
-    console.log(url);
+  const requestOptions = {
+    method: "PUT",
+    body: JSON.stringify(dataToSend),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    const dataToSend = {
-      first_name: inputName.value,
-      last_name: inputLName,
-      email: inputEmail,
-      user_name: inputUserName,
-      password: inputNewPass
-    };
+  fetch(url, requestOptions)
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((data) => {
+      window.location.href = "../main/main.html";
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
-    const requestOptions = {
-      method: 'PUT',
-      body: JSON.stringify(dataToSend),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    };
+document.addEventListener("DOMContentLoaded", (e) => {
+  getUserData()
+    .then((data) => {
+      inputUserName.value = data.user_name;
+      inputUserName.disabled = true;
+      inputName.value = data.first_name;
+      inputName.disabled = true;
+      inputLName.value = data.last_name;
+      inputLName.disabled = true;
+      inputEmail.value = data.email;
+      inputEmail.disabled = true;
+      inputCurrentPass.value = data.password;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-    fetch(url, requestOptions)
-      .then(res=>res.ok?res.json():Promise.reject(res))
-      .then(data => {
-        window.location.href = "../main/main.html";
-        console.log(data);
+document.addEventListener("click", (e) => {
+  // PERMITE EDITAR LOS DATOS DEL FORM
+  e.preventDefault();
+  if (e.target.matches(".btn-editar")) {
+    e.target.previousElementSibling.disabled = false;
+  }
+
+  // MANDA LOS DATOS DEL FORM PARA EL UPDATE
+  if (e.target.matches(".send-data")) {
+    console.log("quisiste mandar data");
+    getUserData()
+      .then((data) => {
+        const userID = data.user_id;
+        updateUser(userID);
+        console.log(userID);
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.log(error);
       });
+  }
+
+  if (e.target.matches(".change-password")){
+    e.preventDefault()
+    errorMessage.classList.add("visible")
+    modal.showModal()
+  }
+
+  if (e.target.matches(".confirm-modal")){
+    e.preventDefault()
+    if (inputNewPass.value !== inputConfirmPass.value){
+      errorMessage.classList.toggle("visible")
+    } else {
+      modal.close()
     }
-})
+  }
 
-
-//        SE PUEDE METER TODO EN EL EVENT DE ARRIBA
-
-// changePassword.addEventListener("click",e=>{
-//   e.preventDefault()
-//   modal.showModal()
-// })
-
-// cerrarModal.addEventListener("click",e=>{
-//   modal.close()
-// })
+  if (e.target.matches(".close-modal")){
+    modal.close()
+    inputNewPass.value = ""
+    inputConfirmPass.value =""
+  }
+});
